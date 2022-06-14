574de1ec7dab7e45ea09ae5c88fb67530945e4c7
UtilityAcc <96780372+UtilityAcc@users.noreply.github.com>
2022-06-14 17:37:13 +0300

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,15 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        cat.voice();
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        cat.voice();
+        System.out.println("Singleton Hello World: " + (bean == bean1));
+        System.out.println("Singleton Cat: " + (cat == cat1));
     }
 }
